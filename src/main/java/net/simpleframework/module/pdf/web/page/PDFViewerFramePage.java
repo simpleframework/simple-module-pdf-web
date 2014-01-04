db7180c6308332961ef5d2a336bf9a2941362ebd
simpleframework <cknet@126.com>
2014-01-04 18:40:11 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/pdf/web/page/PDFViewerFramePage.java
+++ src/main/java/net/simpleframework/module/pdf/web/page/PDFViewerFramePage.java
@@ -16,8 +16,8 @@ import net.simpleframework.mvc.template.AbstractTemplatePage;
 public class PDFViewerFramePage extends AbstractTemplatePage {
 
 	@Override
-	protected void addImportCSS(final PageParameter pp) {
-		super.addImportCSS(pp);
+	protected void onForward(final PageParameter pp) {
+		super.onForward(pp);
 
 		pp.addImportCSS(PDFViewerFramePage.class, "/viewer.css");
 	}
